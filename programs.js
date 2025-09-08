6f285a292dba773aa61fbc68bc261b863842e90e
Hariharan-0511 <hariharan.110501@gmail.com>
2025-09-08 18:01:58 +0530

    updated

--- programs.js
+++ programs.js
@@ -1,11 +1,4 @@
-/*
-1. print the numbers from 1 to 100 with the interval of 10
-2. print the number from 10 to 1
-3. print the multiple of 2 until 10
-4. print the number from 100 to 1 with the difference of 10.
-5. write a function to print the eligble, Not eligble voters separately from the list
-of array [18,17,16,15,10,8,7,9,35,40,86,75,0]. Note: print invalid if the voter age is 0.5. write a function to print the eligble, Not eligble voters separately from the list
-*/
+
 // 1. print the numbers from 1 to 100 with the interval of 10
 console.log("print the numbers from 1 to 100 with the interval of 10:");
 for (let i=0; i<=100;i+=10){
@@ -47,7 +40,8 @@ function voters(arr){
         noteligibleforvoting : noteligible,
         invalidarray: invalid
     }
-    }
+    } 
+
 console.log(voters([18,17,16,15,10,8,7,9,35,40,86,75,0]));
 
 
