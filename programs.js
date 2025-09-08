9d5a43e7cb40d65f6787156fdce645e5ecfdc363
Hariharan-0511 <hariharan.110501@gmail.com>
2025-09-08 18:14:13 +0530

    Updated

--- programs.js
+++ programs.js
@@ -4,21 +4,22 @@ console.log("print the numbers from 1 to 100 with the interval of 10:");
 for (let i=0; i<=100;i+=10){
     console.log(i);
 }
-
+//2.print the number from 10 to 1
 console.log("2.print the number from 10 to 1:");
 for(let i=10;i>=1;i--){
     console.log(i);
 }
-
+//3.print the multiple of 2 until 10
 console.log("3. print the multiple of 2 until 10:");
 for(let i=0 ; i<10; i+=2){
     console.log(i);
 }
-
+//4.print the number from 100 to 1 with the difference of 10
 console.log("4. print the number from 100 to 1 with the difference of 10:");
 for(let i=100 ; i>=1;i-=10){
     console.log(i);
 }
+//5.write a function to print the eligble, Not eligble voters separately from the list of array [18,17,16,15,10,8,7,9,35,40,86,75,0]. Note: print invalid if the voter age is 0
 console.log("5. write a function to print the eligble, Not eligble voters separately from the list of array [18,17,16,15,10,8,7,9,35,40,86,75,0]. Note: print invalid if the voter age is 0.");
 var eligible =[]
 var noteligible = []
